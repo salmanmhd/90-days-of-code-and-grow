e34026f9f4132733133afb598cb050c86b3b0ec9
Md Salman <mhdsalman010@gmail.com>
2024-09-20 07:29:22 +0530

    start far away project

--- Day-21/React-ground/src/main.jsx
+++ Day-21/React-ground/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import Steps from './Steps.jsx';
+// import Steps from './Steps.jsx';
+import DateCounter from './DateCounter.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Steps />
+    {/* <Steps /> */}
+    <DateCounter />
   </StrictMode>
 );

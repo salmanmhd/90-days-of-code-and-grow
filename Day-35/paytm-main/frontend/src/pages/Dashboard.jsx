d631ff1b6352cac398083df03b6dcf0911976e7c
Md Salman <mhdsalman010@gmail.com>
2024-10-12 00:55:26 +0530

    fix fetch data from backend

--- Day-35/paytm-main/frontend/src/pages/Dashboard.jsx
+++ Day-35/paytm-main/frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import Navigation from "../components/Navigation";
 import UserItems from "../components/UserItems";
 function Dashboard() {
   const [name, setName] = useState("");
-  const [balance, setBalance] = useState(500000);
+  const [balance, setBalance] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
     console.log("submitted");
@@ -12,17 +12,24 @@ function Dashboard() {
 
   useEffect(() => {
     async function fetchDetails() {
+      const token = localStorage.getItem("token");
+
       const response = await axios.get(
-        "http://localhost:3000/api/v1/user/balance",
+        "http://localhost:3000/api/v1/account/balance",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
       );
 
-      if (!response) {
-        console.log("error fetching data");
+      if (!response || !response.data) {
+        console.log("Error: Invalid response data");
         return;
       }
-
       setBalance(response.data.balance);
       setName(response.data.firstName);
+      console.log("fetching data");
     }
 
     fetchDetails();
@@ -32,7 +39,11 @@ function Dashboard() {
     <div className="h-screen bg-gradient-to-br from-emerald-950 to-indigo-950">
       <Navigation />
       <div className="ml-10 mt-10 flex h-20 w-96 items-center justify-center rounded-lg border border-emerald-600 text-3xl font-bold">
-        Your Balance: <span className="text-green-400"> ₹{balance}</span>
+        Your Balance:{" "}
+        <span className="text-green-400">
+          {" "}
+          ₹{balance ? Math.round(balance) : 500}
+        </span>
       </div>
       <div className="px-10">
         <div className="my-8 text-3xl">Users</div>
@@ -43,14 +54,11 @@ function Dashboard() {
         />
         <UserItems />
         <UserItems />
-        <UserItems />
-        <UserItems />
-        <UserItems />
-        <UserItems />
-        <UserItems />
       </div>
     </div>
   );
 }
 
 export default Dashboard;
+
+// TODO: fix the amount format
